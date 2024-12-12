bc4516e39e2ff54623750804e23d58d9587486d5
GreeAI <danilaf27@mail.ru>
2024-12-13 01:15:04 +0400

    static_content 1

---
+++ sprint2/problems/static_content/solution/src/request_handler.cpp
@@ -0,0 +1,130 @@
+#include "request_handler.h"
+#include "json_loader.h"
+
+#include <fstream>
+
+namespace http_handler {
+    //Разбить на отдельные методы, добавить валидацию путя
+    RequestHandler::VariantResponse RequestHandler::HandleRequest(StringRequest&& req) {
+        const auto text_response = [this, &req](http::status status, std::string_view text, std::string_view content_type) {
+            return MakeStringResponse(status, text, req.version(), req.keep_alive(), content_type);
+        };
+        const auto file_response = [this, &req](http::status status, http::file_body::value_type& body, std::string_view content_type) {
+            return MakeFileResponse(status, body, req.version(), req.keep_alive(), content_type);
+        };
+        try{
+            if(req.method() == http::verb::get){
+                std::string decoded = URLDecode(req.target().data());
+                std::cout << decoded;
+                if(decoded == "/api/v1/maps") {
+                    const model::Game::Maps maps = game_.GetMaps();
+                    const std::string respons_body = json_loader::MapIdName(maps);
+                    return text_response(http::status::ok, respons_body, ContentType::JSON_HTML);
+                } 
+                else if (StartWithStr(decoded ,"/api/v1/maps/")) {
+                    std::string map_id = decoded.substr(std::string_view("/api/v1/maps/").length());
+                    const model::Game::Maps& maps = game_.GetMaps();
+                    const std::string respons_body = json_loader::MapFullInfo(maps);
+                    if(respons_body.find(map_id) != std::string::npos) {
+                        return text_response(http::status::ok, respons_body, ContentType::JSON_HTML);
+                    }
+                    int status_code = 404;
+                    std::string error_code = json_loader::StatusCodeProcessing(status_code);
+                    return text_response(http::status::not_found, error_code, ContentType::JSON_HTML);
+                }
+                else if (StartWithStr(decoded, "/api")) {
+                    int status_code = 400;
+                    std::string respons_body = json_loader::StatusCodeProcessing(status_code);
+                    return text_response(http::status::bad_request, respons_body, ContentType::JSON_HTML); 
+                }
+                else if (StartWithStr(decoded, "/")) {
+                    decoded == "/" ? decoded = "index.html" : decoded = decoded.substr(1);
+                    if(decoded.empty()) {
+                        return text_response(http::status::not_found, "/", ContentType::TEXT_PLAIN);
+                    }
+                    http::file_body::value_type file;
+                    std::string_view content_type = GetContentType(decoded);
+                    fs::path required_path(decoded);
+                    fs::path summary_path = fs::weakly_canonical(static_path_root_ / required_path);
+                    if (sys::error_code ec; file.open(summary_path.string().data(), beast::file_mode::read, ec), ec) {
+                        std::string empty_body;
+                        return text_response(http::status::not_found, "/", ContentType::TEXT_PLAIN);
+                    }
+                    return file_response(http::status::ok, file, content_type);
+                }
+            }
+            return text_response(http::status::method_not_allowed, "Invalid method", ContentType::JSON_HTML);
+        }
+        catch(const std::exception& e) {
+            return text_response(http::status::internal_server_error, "internal_server_error", ContentType::JSON_HTML);
+        }
+    }
+
+std::string RequestHandler::URLDecode(const std::string& encoded) {
+    std::string decoded;
+    for (size_t i = 0; i < encoded.length(); ++i) {
+        if (encoded[i] == '%' && i + 2 < encoded.length()) {
+            std::string hex = encoded.substr(i + 1, 2);
+            int value = std::stoi(hex, nullptr, 16);
+            decoded += static_cast<char>(value);
+            i += 2;
+        } 
+        else if (encoded[i] == '+') {
+            decoded += ' ';
+        } 
+        else {
+            decoded += encoded[i];
+        }
+    }
+    return decoded;
+}
+
+bool RequestHandler::StartWithStr(const std::string& decoded, const std::string& target) {
+    if (target.size() > decoded.size()) return false;
+    return decoded.compare(0, target.size(), target) == 0;
+}
+
+// Возвращает true, если каталог p содержится внутри base_path.
+bool RequestHandler::IsSubPath(fs::path path, fs::path base) {
+    // Приводим оба пути к каноничному виду (без . и ..)
+    path = fs::weakly_canonical(path);
+    base = fs::weakly_canonical(base);
+
+    // Проверяем, что все компоненты base содержатся внутри path
+    for (auto b = base.begin(), p = path.begin(); b != base.end(); ++b, ++p) {
+        if (p == path.end() || *p != *b) {
+            return false;
+        }
+    }
+    return true;
+}
+
+std::string_view RequestHandler::GetContentType(std::string req_target) {
+        auto point = req_target.find_last_of('.');
+        std::string file_extension = std::string(req_target.substr(point + 1, req_target.npos));
+
+        if (file_extension == "html" || file_extension == "htm") {
+            return RequestHandler::ContentType::TEXT_HTML;
+        }
+        else if (file_extension == "js") {
+            return RequestHandler::ContentType::TEXT_JS;
+        } 
+        else if (file_extension == "txt") {
+            return RequestHandler::ContentType::TEXT_PLAIN;
+        } 
+        else if (file_extension == "json") {
+            return RequestHandler::ContentType::JSON_HTML;
+        } 
+        else if (file_extension == "jpeg" || file_extension == "jpg") {
+            return RequestHandler::ContentType::IMAGE_JPEG;
+        } 
+        else if (file_extension == "svg" || file_extension == "svgz") {
+            return RequestHandler::ContentType::IMAGE_SVG;
+        } 
+        else {
+            return RequestHandler::ContentType::EMPTY;
+        }
+    }
+
+}  // namespace http_handler
+
---
+++ sprint2/problems/static_content/solution/src/request_handler.h
@@ -0,0 +1,95 @@
+#pragma once
+
+#include "http_server.h"
+#include "model.h"
+
+
+#include <string_view>
+#include <map>
+#include <filesystem>
+#include <cassert>
+
+namespace http_handler {
+    
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace fs = std::filesystem;
+namespace sys = boost::system;
+
+using namespace std::literals;
+
+class RequestHandler {
+public:
+
+    explicit RequestHandler(model::Game& game, const std::string& static_root)
+        : game_{game}
+        , static_path_root_{fs::canonical(fs::path(static_root))} {
+    }
+
+    RequestHandler(const RequestHandler&) = delete;
+    RequestHandler& operator=(const RequestHandler&) = delete;
+
+    using StringRequest = http::request<http::string_body>;
+    using StringResponse = http::response<http::string_body>;
+    using FileResponse = http::response<http::file_body>;
+    using VariantResponse = std::variant<StringResponse, FileResponse>;
+
+    template <typename Body, typename Allocator, typename Send>
+    void operator()(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send) {
+        std::visit(
+                [&send, &req](auto&& result) {
+                    send(std::forward<decltype(result)>(result));
+                },
+                HandleRequest(std::forward<decltype(req)>(req)));
+    }
+
+    struct ContentType {
+        ContentType() = delete;
+        constexpr static std::string_view TEXT_HTML = "text/html"sv;
+        constexpr static std::string_view TEXT_JS = "text/javascript"sv;
+        constexpr static std::string_view TEXT_PLAIN = "text/plain"sv;
+        constexpr static std::string_view JSON_HTML = "application/json"sv;
+        constexpr static std::string_view IMAGE_JPEG = "image/jpeg"sv;
+        constexpr static std::string_view IMAGE_SVG = "image/svg+xml"sv;
+        constexpr static std::string_view EMPTY = "application/octet-stream"sv;
+    };
+
+
+    // Создаёт StringResponse с заданными параметрами
+    static StringResponse MakeStringResponse(http::status status, std::string_view body, unsigned http_version,
+                                    bool keep_alive,
+                                    std::string_view content_type) {
+        StringResponse response(status, http_version);
+        response.set(http::field::content_type, content_type);
+        response.body() = body;
+        response.content_length(body.size());
+        response.keep_alive(keep_alive);
+        return response;
+    }
+
+    static FileResponse MakeFileResponse(http::status status, http::file_body::value_type& body, unsigned http_version,
+                                    bool keep_alive,
+                                    std::string_view content_type) {
+        FileResponse response;
+        response.version(http_version);
+        response.result(status);
+        response.insert(http::field::content_type, content_type);
+        response.body() = std::move(body);
+        response.prepare_payload();
+        return response;
+    }
+
+    VariantResponse HandleRequest(StringRequest&& req);
+    std::string URLDecode(const std::string& encoded);
+    bool StartWithStr(const std::string& decoded, const std::string& target);
+
+    bool IsSubPath(fs::path path, fs::path base);
+    std::string_view GetContentType(std::string req_target);
+
+private:
+
+    model::Game& game_;
+    fs::path static_path_root_;
+};
+
+}  // namespace http_handler
